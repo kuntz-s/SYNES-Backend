c969252ac79544e8d34f0f53ac0286fef5d16cad
Micaëlle Grâce NZOUETENG TCHUENTE <96358236+mica-ielle@users.noreply.github.com>
2023-05-30 13:52:53 +0100

    cors update

--- src/main/java/com/synes/SynesApplication.java
+++ src/main/java/com/synes/SynesApplication.java
@@ -3,6 +3,14 @@ package com.synes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SynesApplication {
@@ -11,5 +19,20 @@ public class SynesApplication {
 		SpringApplication.run(SynesApplication.class, args);
 	}
 
+
+	@Bean
+	public FilterRegistrationBean simpleCorsFilter(){
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**",config);
+		FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 
 }
--- src/main/java/com/synes/config/authentification/JwtRequestFilter.java
+++ src/main/java/com/synes/config/authentification/JwtRequestFilter.java
@@ -28,14 +28,14 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws ServletException, IOException {
+/*
 
-
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"/*request.getHeader("Origin")*/);//on peut mettre le nom du domaine a la place de request...
-		response.setHeader("Access-Control-Allow-Credential", "true");
+		response.setHeader("Access-Control-Allow-Origin", "*"/*request.getHeader("Origin")*///);//on peut mettre le nom du domaine a la place de request...
+/*		response.setHeader("Access-Control-Allow-Credential", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Allow-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-
+*/
 
 
 		final String requestTokenHeader = request.getHeader("Authorization");
