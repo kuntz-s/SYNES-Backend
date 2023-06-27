83a6e518bd8a534b6b87042b8681637cd9d2593d
Micaëlle Grâce NZOUETENG TCHUENTE <96358236+mica-ielle@users.noreply.github.com>
2023-06-27 13:51:20 +0100

    Update NotificationControler.java

--- src/main/java/com/synes/controler/notification/NotificationControler.java
+++ src/main/java/com/synes/controler/notification/NotificationControler.java
@@ -2,9 +2,16 @@ package com.synes.controler.notification;
 
 import com.synes.util.Notification;
 import com.synes.util.baseDeDonnee.BaseDeDonnee;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.text.ParseException;
 
 @Controller
 public class NotificationControler {
@@ -24,6 +31,12 @@ public class NotificationControler {
         Thread.sleep(2000);
         return bd.getNotifs();
     }
+
+    @RequestMapping(value = "/getNotifocation", method = RequestMethod.POST, consumes= MediaType.APPLICATION_JSON_VALUE)
+    public Object getNot(@RequestHeader("authorization") String token, HttpServletResponse response) throws InterruptedException, ParseException {
+        return getNotif();
+    }
+
 
     @MessageMapping("/sendNotificationto")
     @SendTo("/topic/sendNotificationto")
